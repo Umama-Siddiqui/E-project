2178e8645c05b012874c7dd8e5115bcb6ea5171b
umamasiddiqui <umamasiddiqui719@gmail.com>
2025-05-13 02:58:00 +0500

    ya hay main file

--- courier-system/phpwork/nav.php
+++ courier-system/phpwork/nav.php
@@ -17,17 +17,17 @@
 <!-- Profile Modal -->
 <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-contents bg-secondary text-white">
-      <div class="modal-header border-0">
+    <div class="modal-content custom-modal">
+      <div class="modal-header custom-modal-header">
         <h5 class="modal-title" id="profileModalLabel">Admin Profile</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body custom-modal-body">
         <p><strong>Name:</strong> Admin User</p>
         <p><strong>Email:</strong> admin@example.com</p>
       </div>
-      <div class="modal-footer border-0">
-        <button type="button" class="btn btn-outline-light" data-bs-dismiss="modal">Close</button>
+      <div class="modal-footer custom-modal-footer">
+        <button type="button" class="btn btn-outline-light custom-close-btn" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
